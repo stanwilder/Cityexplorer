9924fc20c13e458408c7a64ac2de79ac1298dc7c
Stanley <stanley.wilder.24@gmail.com>
2022-05-16 15:43:48 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityName: '',
+    }
+  }
 
-export default App;
+  citySubmit = async (e) => {
+    e.preventDefault();
+    let apiUrl = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATION_API_KEY}&q${this.state.city}&format=json`;
+    let city = await axios.get(apiUrl);
+    console.log(city);
+  }
+  
+  render() {
+    return (
+      <>
+      <Form>
+        <Form.Label>Explore</Form.Label>
+        <Button type="submit">
+          Explore!
+        </Button>
+      </Form>
+      </>
+      );
+    }
+    
+  }
+    export default App;
+    
+  // <div className="App">
+  //   <header className="App-header">
+  //     <img src={logo} className="App-logo" alt="logo" />
+  //     <p>
+  //       Edit <code>src/App.js</code> and save to reload.
+  //     </p>
+  //     <a
+  //       className="App-link"
+  //       href="https://reactjs.org"
+  //       target="_blank"
+  //       rel="noopener noreferrer"
+  //     >
+  //       Learn React
+  //     </a>
+  //   </header>
+  // </div>
